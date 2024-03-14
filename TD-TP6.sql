629befa2b0103a4a48d059c248d2f6beba17ee8d
Patrique29 <baka.coca@gmail.com>
2024-03-14 09:23:27 +0100

    Q1 terminé

--- TD-TP6.sql
+++ TD-TP6.sql
@@ -10,14 +10,19 @@ DROP USER rian@localhost;
 
 SHOW DATABASES;
 
-DROP DATABASE bd_r206;
-DROP DATABASE bd_r206_avion;
-DROP DATABASE bd_r206_banque;
-DROP DATABASE bd_r206_iutapprentissage;
+DROP DATABASE db_r206;
+DROP DATABASE db_r206_avion;
+DROP DATABASE db_r206_banque;
+DROP DATABASE db_r206_iutapprentissage;
 
 --.2
-CREATE DATABASE bd_iut;
+CREATE DATABASE db_iut;
 CREATE USER 'pham'@'localhost' IDENTIFIED BY 'mdp_pham';
 CREATE ROLE 'DBA';
 GRANT ALL PRIVILEGES ON *.* TO 'DBA' WITH GRANT OPTION;
 GRANT 'DBA' TO 'pham'@'localhost' ;
+
+--.3
+USE db_iut;
+\. "C:\Users\Rayanne\Downloads\R206_TP1_Creation_MySQL.sql"
+\. "C:\Users\Rayanne\Downloads\R206_TP1_Remplissage_MySQL.sql"
