33cb5ccaba5d82a91299711d678acd8099f96ca6
Patrique29 <baka.coca@gmail.com>
2024-03-14 08:41:58 +0100

    Question 1.1 répondu

--- TD-TP6.sql
+++ TD-TP6.sql
@@ -2,3 +2,17 @@
 Coignard Maël
 Mellah Rayanne
 */
+
+--Q1
+--.1
+SELECT user, host FROM mysql.user;
+DROP USER rian@localhost;
+
+SHOW DATABASES;
+
+DROP DATABASE bd_r206;
+DROP DATABASE bd_r206_avion;
+DROP DATABASE bd_r206_banque;
+DROP DATABASE bd_r206_iutapprentissage;
+
+--.2
