9a8f6b725ee6b4b72b53a70a0d0423b59385a3b2
Patrique29 <baka.coca@gmail.com>
2024-03-19 10:01:21 +0100

    modified:   TD-TP6.sql

--- TD-TP6.sql
+++ TD-TP6.sql
@@ -26,3 +26,18 @@ GRANT 'DBA' TO 'pham'@'localhost' ;
 USE db_iut;
 \. "C:\Users\Rayanne\Downloads\R206_TP1_Creation_MySQL.sql"
 \. "C:\Users\Rayanne\Downloads\R206_TP1_Remplissage_MySQL.sql"
+
+
+--Q2
+--.4
+CREATE OR REPLACE VIEW vue_Etud_Stag_Ent AS
+SELECT idEtud FROM Etudiant
+JOIN Stagiaire ON etudStagiaire = idEtud
+JOIN Entreprise ON idEntreprise = entrepriseStage
+WITH CHECK OPTION;
+
+CREATE OR REPLACE VIEW vue_Etud_App_Ent AS
+SELECT idEtud FROM Etudiant
+JOIN Apprenti ON etudApp = idEtud
+JOIN Entreprise ON idEntreprise = entrepriseApp
+WITH CHECK OPTION;
