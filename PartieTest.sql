9820ad6290642898b4bab4f4c659a4f62c5655d4
Patrique29 <baka.coca@gmail.com>
2024-03-19 14:20:34 +0100

    quastion faite pas répondu parcontre car  fleme

--- PartieTest.sql
+++ PartieTest.sql
@@ -7,10 +7,10 @@ Mellah Rayanne
 
 
 --testKamp
---Enlever Tuffigo de la base de donnée
-SELECT Enseignant
+--Supprimer Tuffigo de la base de donnée et remplacer toutes ses apparation avec Raut
 
 
+--(Erreur attendu) Donner les droit d'écriture dans toutes la base à Fleurquin (fleurquin@localhost)
 
 
 
@@ -19,9 +19,11 @@ SELECT Enseignant
 
 
 --testKhayata
+--Changer le tuteur du groupe C pour mettre Philippe Tonin
 
 
 
+--(Erreur attendu) Ajouter le Médiateur Européen d'id 72 dans Entreprise dans le département 67 
 
 
 
@@ -37,3 +39,7 @@ SELECT Enseignant
 
 
 --testFleurquin
+--Changer la poursuite d'étude de Laliman Simon pour mettre Licence Info Vannes
+
+
+--(Erreur attendu) Changer le dossier de Laliman Simon pour mettre qu'il a fait une Formation Initial en BUT 2 et qu'il a pris le parcours IA
