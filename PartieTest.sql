9b3fa6d9d23c799c43a826e19c1078342853f057
Patrique29 <baka.coca@gmail.com>
2024-03-19 11:15:19 +0100

    modified:   PartieTest.sql

--- PartieTest.sql
+++ PartieTest.sql
@@ -2,3 +2,38 @@
 Coignard Maël
 Mellah Rayanne
 */
+
+
+
+
+--testKamp
+--Enlever Tuffigo de la base de donnée
+SELECT Enseignant
+
+
+
+
+
+
+
+
+
+--testKhayata
+
+
+
+
+
+
+
+
+--testBaudont
+
+
+
+
+
+
+
+
+--testFleurquin
