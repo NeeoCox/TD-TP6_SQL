b165f91a35d8d5e0bd47de06cea9ac2e3f5897a2
NeeoCox <maelcoignard16@gmail.com>
2024-03-19 14:48:10 +0100

    new file:   TD-TP6_Test.sql 	modified:   TD-TP6.sql

---
+++ TD-TP6_Test.sql
@@ -0,0 +1,12 @@
+/*suprimer le groupe C avec kamp*/
+
+/*Modifier un poursuite d'etude avec fleurquin puis avec baudont */
+
+/*lire la vu avec baudont et et khayata */
+
+/*ajouter l'etudiant olimar oatchi dans le groupe b*/
+
+/*ajouter le groupe test avec khayata */
+
+
+
